e9d2be29baf6528cd4fb6761f28eedaeb382303d
heeguk <48238788+heeguk@users.noreply.github.com>
2019-03-27 21:51:22 +0900

    Update CSVstreaming.java

--- CSVstreaming.java
+++ CSVstreaming.java
@@ -13,11 +13,13 @@ import org.apache.spark.sql.Row;
 
 public final class CSVstreaming {
 public static void main(String[] args) throws Exception {
+		//구조적 스트리밍 사용하기 위해 스파크세션 생성
 		SparkSession spark = SparkSession
 			  .builder()
 			  .appName("JavaStructuredNetworkWordCount")
 			  .getOrCreate();
 		
+		// .csv 파일을 받아오기 전에 Dataset의 스키마를 만들어야함
 		StructType MySchema = DataTypes.createStructType(new StructField[] {
 	            DataTypes.createStructField("id",  DataTypes.StringType, true),
 	            DataTypes.createStructField("name", DataTypes.StringType, true),
@@ -25,7 +27,7 @@ public static void main(String[] args) throws Exception {
 	            DataTypes.createStructField("age", DataTypes.IntegerType, true)
 	    });
 		
-		
+		// 해당 경로에 있는 .csv 파일을 모두 가져옴. 파일의 스키마는 기존에 정의한 스키마를 따름
 		Dataset<Row> df = spark
 				  .readStream()
 				  .format("csv")
@@ -34,7 +36,7 @@ public static void main(String[] args) throws Exception {
 				  .csv("/home/heeguk/csvhere");
 
 		
-		
+		//콘솔에 
 		StreamingQuery query = df.writeStream()
 				  .outputMode("append")
 				  .format("console")
